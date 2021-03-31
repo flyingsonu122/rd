cc1f8ee240fe267e0a1e58b1072d00285b6197c3
Sonu kumar kushwaha <flyingsonu736@gmail.com>
2021-03-31 21:36:19 +0530

    first commit

---
+++ rd.js
@@ -0,0 +1,34 @@
+// var url = 'https://www.rd.com/feed/'
+
+const textarea = document.querySelector('#feed-textarea > ul');
+
+const date = new Date();
+document.querySelector('#date').innerHTML = date.toDateString();
+
+var url = 'https://www.rd.com/feed/atom/'
+feednami.load(url, function (result) {
+    if (result.error) {
+        console.log(result.error)
+    }
+    else {
+        var entries = result.feed.entries
+        for (var i = 0; i < entries.length; i++) {
+            var entry = entries[i]
+
+            // console.log(entry.title)
+            // console.log(entry.summary)
+
+            let li = document.createElement('li');
+            let para = document.createElement('p');
+
+            //add HTML content to list items
+            li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4>`;
+            para.innerHTML = entry.summary;
+
+
+            //append HTML content to list 
+            textarea.appendChild(li);
+            textarea.appendChild(para);
+        }
+    }
+})
