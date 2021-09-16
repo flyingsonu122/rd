8176f1aa9b8e525faa6c59ed712d8be411e37d0f
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-16 13:00:52 +0530

    Update rd.js

--- rd.js
+++ rd.js
@@ -5,9 +5,11 @@ const textarea = document.querySelector('#feed-textarea > ul');
 const date = new Date();
 document.querySelector('#date').innerHTML = date.toDateString();
 
+var url = 'https://www.rd.com/feed/atom/'
 
 
-var url = 'https://buttondown.email/cassidoo/rss'
+
+
 
 feednami.load(url, function (result) {
     if (result.error) {
